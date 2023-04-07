abbf820658e97bcf3210f61453264f39c54668c2
LucasCruz <lca.snf21@uea.edu.br>
2023-04-07 01:30:32 -0400

    update

---
+++ Assets/ScriptableObjects/GiantAttributes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GiantAttributes", menuName = "The Giant Night/GiantAttributes")]
+public class GiantAttributes : ScriptableObject
+{
+    public int lifeGiant;
+    public float damageAttack;
+}
---
+++ Assets/ScriptableObjects/PlayerAttributes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerAttributes", menuName = "The Giant Night/PlayerAttributes")]
+public class PlayerAttributes : ScriptableObject
+{
+    public int lifePlayer;
+    public int manaPlayer;
+    public float damageEletricity;
+}

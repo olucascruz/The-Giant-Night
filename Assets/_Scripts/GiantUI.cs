79997af73b5817685f98e2310fdcee3a11919e8d
LucasCruz <lca.snf21@uea.edu.br>
2023-04-09 01:13:50 -0400

    fix: adjust enemy UI

--- Assets/_Scripts/GiantUI.cs
+++ Assets/_Scripts/GiantUI.cs
@@ -6,8 +6,7 @@ using UnityEngine.UI;
 public class GiantUI : MonoBehaviour
 {
 
-    [SerializeField]
-    private GiantAttributes giantAttributes;
+    [SerializeField] private GiantAttributes giantAttributes;
     
     private int maxHealth;
     private int currentHealth;
@@ -15,23 +14,30 @@ public class GiantUI : MonoBehaviour
     [SerializeField] private Image healthBar;
     
     private GameObject player;
+    [SerializeField] private GameObject giant;
+
+    private Enemy enemy;
+
+    private int life;
     
     // Start is called before the first frame update
     void Start()
     {
+        enemy = giant.GetComponent<Enemy>();
         player = GameObject.FindWithTag("Player");
         maxHealth = giantAttributes.lifeGiant;    
         healthBar.fillAmount = 1;   
     }
 
     public float GetHealthAsFraction() {
-        return Mathf.Clamp01((float) giantAttributes.lifeGiant / maxHealth);
+        return Mathf.Clamp01((float) life / maxHealth);
     }
 
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        life = enemy.EnemyLife;
         transform.LookAt(player.transform);
         healthBar.fillAmount = GetHealthAsFraction();
     }

8dd09db0de57678c6807bb4f69635a98f5621e86
LucasCruz <lca.snf21@uea.edu.br>
2023-04-10 15:07:35 -0400

    update

--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -11,41 +11,46 @@ public class PlayerUI : MonoBehaviour
     
     private int maxHealth, currentHealth;
 
-    [SerializeField]
-    private Image healthBar;
+
+    [SerializeField] private Image healthBar;
     
-    [SerializeField]
-    private Image[] manaBar;
+    [SerializeField] private Image[] manaBar;
 
+    [SerializeField] private Player player;
 
+    private int health;
+    private int playerMana; 
     void Start()
     {
-     maxHealth = playerAttributes.lifePlayer;
-    
+     player = player.GetComponent<Player>();
+     maxHealth = player.PlayerLife;
      healthBar.fillAmount = 1;
     }
 
     public float GetHealthAsFraction() {
-        return Mathf.Clamp01((float) playerAttributes.lifePlayer / maxHealth);
+        return Mathf.Clamp01((float) health / maxHealth);
     }
 
     void Update()
     {
+        health = player.PlayerLife;
+        playerMana = player.PlayerMana;
         healthBar.fillAmount = GetHealthAsFraction();
-        if(playerAttributes.manaPlayer == 3){
+        
+        if(playerMana == 3){
             foreach (var item in manaBar)
             {
                 item.enabled = true;
             }
-        }else if(playerAttributes.manaPlayer == 2){
+        }else if(playerMana == 2){
             manaBar[0].enabled = false;
             manaBar[1].enabled = true;
             manaBar[2].enabled = true;
-        }else if(playerAttributes.manaPlayer == 1){
+        }else if(playerMana == 1){
             manaBar[0].enabled = false;
             manaBar[1].enabled = false;
             manaBar[2].enabled = true;
-        }else if(playerAttributes.manaPlayer == 0){
+        }else if(playerMana == 0){
             foreach (var item in manaBar)
             {
                 item.enabled = false;
